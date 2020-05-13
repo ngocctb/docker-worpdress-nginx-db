1ea01a2d49db320bed6d992f41fc8fea9453bf0e
Chau Trung Bao Ngoc <chautrungbaongoc@gmail.com>
2020-05-13 16:25:35 +0700

    init test connecting to DB

--- cms/index.php
+++ cms/index.php
@@ -1,2 +1,40 @@
-<?php 
-phpinfo();
+<?php
+
+$mysqli = new mysqli('db', 'root', 'root', 'wordpress');
+
+// Check connection
+if (!$mysqli) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+// Attempt select query execution
+$sql = "SELECT * FROM test;";
+$result = mysqli_query($mysqli, $sql);
+if($result){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>name</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['name'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+}
+
+$mysqli->close();
+?>
